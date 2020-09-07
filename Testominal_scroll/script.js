ecf37751126e8448f666b4ae81f235c9eaf8e186
NidhitheCoder <nidhpv@gmail.com>
2020-09-07 22:27:12 +0530

    formating

--- Testominal_scroll/script.js
+++ Testominal_scroll/script.js
@@ -1,17 +1,17 @@
 import Swiper from 'https://unpkg.com/swiper/swiper-bundle.esm.browser.min.js';
-let swiper = new Swiper(".swiper-container",{
-    effect:'coverflow',
-    grabCursor:true,
-    centeredSlides:true,
-    slidesPerView:'auto',
-    coverFlowEffect:{
-        rotate:50,
-        stretch:0,
-        depth:100,
-        modifier:1,
-        slideShadows:true,
+let swiper = new Swiper(".swiper-container", {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverFlowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
     },
-    pagination:{
-        el:'.swiper-pagination',
+    pagination: {
+        el: '.swiper-pagination',
     },
 });
