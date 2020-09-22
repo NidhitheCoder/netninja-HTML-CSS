2b196f4ef16287f77a79009e67a7f0171d2b2883
NidhitheCoder <nidhpv@gmail.com>
2020-09-22 23:11:15 +0530

    change using js

--- animatedPage/script.js
+++ animatedPage/script.js
@@ -10,5 +10,17 @@ body.addEventListener("mousemove", (e) => {
   y = y * 3;
   sum = x + y;
   elm.style.color = `rgb(${x},${y},${sum > 255 ? 255 - sum : sum})`;
-  elm.style.background = `rgb(${sum > 255 ? 255 - sum : sum},${x},${y})`
+  elm.style.background = `rgb(${sum > 255 ? 255 - sum : sum},${x},${y})`;
+
+  let num1 = Math.floor(Math.random() * 10);
+  let num2 = Math.floor(Math.random() * 10);
+  let circles = document.querySelectorAll(".circle");
+
+  circles = [...circles];
+
+  for(let i=0;i<circles.length;i++){
+    let circle = circles[i];
+    circle.style.top = num1 * i ;
+    circle.style.left = num2 * i ;
+  }
 });
