e6a128e5710a191d8eb8506e5d400bbc50d6f080
NidhitheCoder <nidhpv@gmail.com>
2020-10-07 13:44:15 +0530

    horizontal scroll completed

--- HScroll_With_SkewEffect/script.js
+++
@@ -1,8 +0,0 @@
-let blocks = document.getElementsByClassName('block');
-let container = document.getElementsByClassName('container');
-let hs = new HorizontalScroll.default({
-    blocks:blocks,
-    container:container,
-    isAnimated:true,
-    springEffect:0.8
-});
